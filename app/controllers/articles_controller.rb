2faf3069bcfc3a5b6c4e19e67068d9177a8a482b
Travis Montgomery <travisbm@gmail.com>
2016-02-16 16:48:20 -0600

    adding CRUD to articles controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,14 @@
 class ArticlesController < ApplicationController
 def index
+  @articles = Article.all
 end
 
 def show
+  @article = Article.find(params[:id])
 end
 
 def new
+  @article = Article.new
 end
 
 def edit
@@ -14,8 +17,11 @@ end
 def create
   @article = Article.new(article_params)
 
-  @article.save
-  redirct_to @article
+   if @article.save
+     redirect_to @article
+   else
+     render 'new'
+   end
 end
 
 def update
