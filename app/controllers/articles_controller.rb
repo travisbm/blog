734086781d07edc308ee0ea8781a2c1a1e55bd52
Travis Montgomery <travisbm@gmail.com>
2016-02-16 17:49:49 -0600

    updating views. added shared form

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -12,6 +12,7 @@ def new
 end
 
 def edit
+  @article = Article.find(params[:id])
 end
 
 def create
@@ -25,9 +26,20 @@ def create
 end
 
 def update
+  @article = Article.find(params[:id])
+
+  if @article.update(article_params)
+    redirect_to @article
+  else
+    render 'edit'
+  end
 end
 
 def destroy
+  @article = Article.find(params[:id])
+  @article.destroy
+
+  redirect_to articles_path
 end
 
 private
