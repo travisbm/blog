1611100a39a49bb43ad174865568f411d5a9dc25
Travis Montgomery <travisbm@gmail.com>
2016-02-16 16:03:46 -0600

    initial commit

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,32 @@
+class ArticlesController < ApplicationController
+def index
+end
+
+def show
+end
+
+def new
+end
+
+def edit
+end
+
+def create
+  @article = Article.new(article_params)
+
+  @article.save
+  redirct_to @article
+end
+
+def update
+end
+
+def destroy
+end
+
+private
+  def article_params
+    params.require(:article).permit(:title, :text)
+  end
+
+end
